82fc894cc2944d37512230dc1c541115d14a6e5d
Zulfa Iz <zulfaizR@gmail.com>
2021-10-17 21:50:42 +0700

    first commit

---
+++ app/src/main/java/com/example/a1918066_tugas_layout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a1918066_tugas_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
